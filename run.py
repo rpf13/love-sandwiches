ff73a9b73d676a3c059d9421a987a49d60a5ab1d
rpf13 <rpf13@me.com>
2022-12-20 20:54:59 +0000

    Refactor update worksheet functions into one

--- run.py
+++ run.py
@@ -63,14 +63,34 @@ def validate_data(values):
     return True
 
 
-def update_sales_worksheet(data):
+# def update_sales_worksheet(data):
+#     """
+#     Update sales worksheet, add new row with the list of data provided.
+#     """
+#     print("Updating sales worksheet...\n")
+#     sales_worksheet = SHEET.worksheet('sales')
+#     sales_worksheet.append_row(data)
+#     print("Sales worksheet updated successfully.\n")
+# 
+# 
+# def update_surplus_worksheet(data):
+#     """
+#     Update surplus worksheet, add new row with the list of data provided.
+#     """
+#     print("Updating surplus worksheet...\n")
+#     surplus_worksheet = SHEET.worksheet('surplus')
+#     surplus_worksheet.append_row(data)
+#     print("Surplus worksheet updated successfully.\n")
+
+def update_worksheet(data, worksheet):
     """
-    Update sales worksheet, add new row with the list of data provided.
+    Receives a list of integers to be insterted into a worksheet
+    Update the relevant worksheet with the data provided
     """
-    print("Updating sales worksheet...\n")
-    sales_worksheet = SHEET.worksheet('sales')
-    sales_worksheet.append_row(data)
-    print("Sales worksheet updated successfully.\n")
+    print(f"Updating {worksheet} worksheet...\n")
+    worksheet_to_update = SHEET.worksheet(worksheet)
+    worksheet_to_update.append_row(data)
+    print(f"{worksheet} worksheet updated successfully\n")
 
 
 def calculate_surplus_data(sales_row):
@@ -93,17 +113,6 @@ def calculate_surplus_data(sales_row):
     
     return surplus_data
 
-
-def update_surplus_worksheet(data):
-    """
-    Update surplus worksheet, add new row with the list of data provided.
-    """
-    print("Updating surplus worksheet...\n")
-    surplus_worksheet = SHEET.worksheet('surplus')
-    surplus_worksheet.append_row(data)
-    print("Surplus worksheet updated successfully.\n")
-
-
 # The main function to call all other functions is usually wrapped into a main function.
 # Then the main function gets called.
 def main():
@@ -112,10 +121,10 @@ def main():
     """
     data = get_sales_data()
     sales_data = [int(num) for num in data]
-    update_sales_worksheet(sales_data)
+    update_worksheet(sales_data, "sales")
     new_surplus_data = calculate_surplus_data(sales_data)
     print(new_surplus_data)
-    update_surplus_worksheet(new_surplus_data)
+    update_worksheet(new_surplus_data, "surplus")
 
 print("Welcome to love Sandwiches Data Automation")
 main()
