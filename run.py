71d8e56999de088212e5a65274295e0c884a44ff
rpf13 <rpf13@me.com>
2022-12-21 07:41:28 +0000

    Calculate stock numbers and add to stock worksheet incl. 10% bonus stock

--- run.py
+++ run.py
@@ -115,6 +115,25 @@ def get_last_5_entries_sales():
     return columns
 
 
+def calculate_stock_data(data):
+    """
+    Calculate the average stock for each item type, adding 10%
+    """
+    print("Calculating stock data...\n")
+    new_stock_data = []
+
+    for column in data:
+        # convert values in integers
+        int_column = [int(num) for num in column]
+        average = sum(int_column) / len(int_column)
+        # add 10% extra stock
+        stock_num = average * 1.1
+        new_stock_data.append(round(stock_num))
+    
+    return new_stock_data
+
+
+
 # The main function to call all other functions is usually wrapped into a main function.
 # Then the main function gets called.
 def main():
@@ -127,8 +146,9 @@ def main():
     new_surplus_data = calculate_surplus_data(sales_data)
     print(new_surplus_data)
     update_worksheet(new_surplus_data, "surplus")
+    sales_columns = get_last_5_entries_sales()
+    stock_data = calculate_stock_data(sales_columns)
+    update_worksheet(stock_data, "stock")
 
 print("Welcome to love Sandwiches Data Automation")
-# main()
-
-sales_columns = get_last_5_entries_sales()
+main()
